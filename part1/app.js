e655381f4952df7cb73b6443979752732dfcf564
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 08:35:03 +0000

    [automatic commit] 2025-06-20 08:35:03 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -189,6 +189,10 @@ let db;
 
 // * Routes
 // * Testing querries can be found Q6-8.sql
+
+// ? Maybe we should select a list of walker user names
+// ? Then have two querries selecting completed walks and ratings that is mapped based on username -> object
+// ? We can then process it by looping through the usernames
 app.get('/api/walkers/summary', async(req,res) => {
     try {
         let rows;
