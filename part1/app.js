333106b40084f9b00fcd9f4977162ec614bfee54
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:33:06 +0000

    [automatic commit] 2025-06-20 06:33:06 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -114,6 +114,7 @@ app.use(express.static(path.join(__dirname, 'public')));
                     ( 'bobwalker', 'bob@example.com', 'hashed456', 'walker' ),
                     ( 'carol123', 'carol@example.com', 'hashed789', 'owner '),
                     ( 'kinglouisXIV', 'theking@example.com', 'hashed1214', 'owner' ),
+                    ( 'zewalkerz', 'zewalkerz@example.com', 'hashed1519', 'walker'),
                     ( 'zewalkerz', 'zewalkerz@example.com', 'hashed1519', 'walker');
             `);
         }
@@ -154,9 +155,9 @@ app.use(express.static(path.join(__dirname, 'public')));
                 INSERT INTO WalkRequests (request_id, walker_id, owner_id, rating, comments ) VALUES
                     ( 2, (SELECT user_id FROM users WHERE username = 'bobwalker'),  ,),
                     ( 3, (SELECT user_id FROM users WHERE username = 'bobwalker'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' ),
-                    ( 4, (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Belfort' LIMIT 1), '2025-06-15 08:30:00', 60, 'Versailles', 'open' ),
-                    ( 5, (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Lupin' LIMIT 1), '2025-06-15 09:00:00', 30, 'Versailles', 'accepted' ),
-                    ( 6, (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Fluke' LIMIT 1), '2025-06-18 09:00:00', 45, 'Fort Funston', 'cancelled' );
+                    ( 4, (SELECT user_id FROM users WHERE username = 'bobwalker'), '2025-06-15 08:30:00', 60, 'Versailles', 'open' ),
+                    ( 5, (SELECT user_id FROM users WHERE username = 'bobwalker'), '2025-06-15 09:00:00', 30, 'Versailles', 'accepted' ),
+                    ( 6, (SELECT user_id FROM users WHERE username = 'bobwalker'), '2025-06-18 09:00:00', 45, 'Fort Funston', 'cancelled' );
             `);
         }
 
