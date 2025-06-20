bbfa2cb3a5d37c32790d36cff83550c39eaf0fc7
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 16:54:35 +0000

    [automatic commit] 2025-06-20 16:54:35 UTC 203.59.207.151

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -97,7 +97,7 @@ router.post('/getOwnerIDs', async (req,res) => {
     FROM Users
     WHERE username IN ( ? )`;
   try {
-    // * Fun fact this ONLY works in here 
+    // * Fun fact this ONLY works in here db.execute will not for some reason at all
     db.pool.getConnection(function(err1,connection){
       if(err1){
         return;
