db48664815937e22482541f93b7de1543ad3710a
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:47:05 +0000

    [automatic commit] 2025-06-20 06:47:05 UTC 203.59.207.151

---
+++ part1/Q6-8.sql
@@ -0,0 +1,8 @@
+INSERT INTO Users ( username, email, password_hash, role ) VALUES
+    ( 'alice123', 'alice@example.com', 'hashed123', 'owner' ),
+    ( 'bobwalker', 'bob@example.com', 'hashed456', 'walker' ),
+    ( 'carol123', 'carol@example.com', 'hashed789', 'owner '),
+    ( 'kinglouisXIV', 'theking@example.com', 'hashed1214', 'owner' ),
+    ( 'zewalkerz', 'zewalkerz@example.com', 'hashed1519', 'walker'),
+    ( 'walker', 'walker2@example.com', 'hashed2021', 'walker');
+
