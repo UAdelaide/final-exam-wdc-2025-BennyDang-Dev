4c187ae04cf4fa662498529edbbf266e1393deea
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 07:50:34 +0000

    [automatic commit] 2025-06-20 07:50:34 UTC 203.59.207.151

--- part1/Q6-8.sql
+++ part1/Q6-8.sql
@@ -64,8 +64,8 @@ INNER JOIN Users U ON WRT.walker_id = U.user_id
 INNER JOIN WalkRequests WR ON WRT.request_id = WR.request_id
 GROUP BY WRT.walker_id;
 
-SELECT 
+SELECT *
 FROM WalkApplications WA
 INNER JOIN WalkRequests WR ON WA.request_id = WR.request_id
-RIGHT JOIN
+RIGHT JOIN Users U ON WA.walker_id = 
 WHERE WA.status = 'accepted' AND WR.status = 'completed';
