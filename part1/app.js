7637f53329b3f2307a6101ed766eb9facaef6be7
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:42:39 +0000

    [automatic commit] 2025-06-20 06:42:39 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -152,7 +152,7 @@ app.use(express.static(path.join(__dirname, 'public')));
         [rows] = await db.execute(`SELECT COUNT(*) AS count FROM WalkRatings`);
         if(rows[0].count === 0){
             await db.execute(`
-                INSERT INTO WalkRequests (request_id, walker_id, owner_id, rating, comments ) VALUES
+                INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments ) VALUES
                     ( 2, (SELECT user_id FROM Users WHERE username = 'bobwalker'), 1 , 5 , 'some comments'),
                     ( 3, (SELECT user_id FROM Users WHERE username = 'bobwalker'), 3 , 4 , 'some comments'),
                     ( 4, (SELECT user_id FROM Users WHERE username = 'zewalkerz'), 4 , 3 , ''),
