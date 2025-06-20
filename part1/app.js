bf0bda078f4a6174e77976d5e204c0f518d71698
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:03:14 +0000

    [automatic commit] 2025-06-20 06:03:14 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -40,12 +40,18 @@ app.use(express.static(path.join(__dirname, 'public')));
         const [rows] = await db.execute(`SELECT COUNT(*) AS count FROM Users`);
         if(rows[0].count === 0){
             await db.execute(`
-                
+                INSERT INTO Users ( username, email, password_hash, role ) VALUES
+                    ( 'alice123', 'alice@example.com', 'hashed123', 'owner' ),
+                    ( 'bobwalker', 'bob@example.com', 'hashed456', 'walker' ),
+                    ( 'carol123', 'carol@example.com', 'hashed789', 'owner '),
+                    ( 'kinglouisXIV', 'theking@example.com', 'hashed1214', 'owner' ),
+                    ( 'zewalkerz', 'zewalkerz@example.com', 'hashed1519', 'walker');
             `);
+
         }
 
     }catch(error){
-        process.stdout.write('Check that DogWalkService DB Exist! \n ');
+        process.stdout.write(' \n ');
         console.error(error);
     }
 })();
