ebced5cb5a0455b8c6c34501f1c3104a2c152cd0
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 15:09:13 +0000

    [automatic commit] 2025-06-20 15:09:13 UTC 203.59.207.151

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -28,6 +28,7 @@ router.get('/ownerRequests', async (req, res) => {
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
+      WHERE u.user_id = ?
       ORDER BY 
       `;
   try{
