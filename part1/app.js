0c7df5fbea537385b7b6ac03abca89183a397627
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:26:36 +0000

    [automatic commit] 2025-06-20 06:26:36 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -136,12 +136,12 @@ app.use(express.static(path.join(__dirname, 'public')));
         if(rows[0].count === 0){
             await db.execute(`
                 INSERT INTO WalkRequests ( dog_id, requested_time, duration_minutes, location, status ) VALUES
-                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Fluke' LIMIT 1), '2025-06-02 08:00:00', 45, 'Fort Funston', 'cancelled' )
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Fluke' LIMIT 1), '2025-06-02 08:00:00', 45, 'Fort Funston', 'cancelled' ),
                     ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max' LIMIT 1), '2025-06-10 08:00:00', 30, 'Parklands', 'completed' ),
                     ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'completed' ),
                     ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Belfort' LIMIT 1), '2025-06-15 08:30:00', 60, 'Versailles', 'open' ),
                     ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Lupin' LIMIT 1), '2025-06-15 09:00:00', 30, 'Versailles', 'completed' ),
-                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Fluke' LIMIT 1), '2025-06-18 09:00:00', 45, 'Fort Funston', 'completed' ),
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Fluke' LIMIT 1), '2025-06-18 09:00:00', 45, 'Fort Funston', 'completed' )
             `);
         }
 
