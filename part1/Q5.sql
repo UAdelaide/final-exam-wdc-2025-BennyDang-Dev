882a54436795d71a3b212e9e629569da4475e7ac
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 04:08:29 +0000

    [automatic commit] 2025-06-20 04:08:29 UTC 203.59.207.151

--- part1/Q5.sql
+++ part1/Q5.sql
@@ -18,6 +18,6 @@ INSERT INTO Dogs ( owner_id, name, size ) VALUES
 INSERT INTO WalkRequests ( dog_id, requested_time, duration_minutes, location, status ) VALUES
     ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max' LIMIT 1), '2025-06-10 08:00:00', 30, 'Parklands', 'open' ),
     ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' ),
-    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Belfort' LIMIT 1), '2025-06-15 08:30:00', 60, 'Versailles', 'completed' ),
+    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Belfort' LIMIT 1), '2025-06-15 08:30:00', 60, 'Versailles', 'open' ),
     ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Lupin' LIMIT 1), '2025-06-15 09:00:00', 30, 'Versailles', 'accepted' ),
-    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' );
+    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Fluke' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' );
