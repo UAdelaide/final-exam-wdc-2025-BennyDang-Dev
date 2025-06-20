6b5f765212ca8be5902f28d2231b0b3962b8fda9
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:50:30 +0000

    [automatic commit] 2025-06-20 06:50:30 UTC 203.59.207.151

--- part1/Q6-8.sql
+++ part1/Q6-8.sql
@@ -32,6 +32,6 @@ INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments ) VAL
     ( 6, (SELECT user_id FROM Users WHERE username = 'zewalkerz'), 1 , 5 , '');
 
 -- * Some Test Querries
-SELECT name, size, U.user_name
+SELECT name, size, U.username
 FROM Dogs D
-INNER JOIN User U ON D.owner_id = U.user_id;
+INNER JOIN Users U ON D.owner_id = U.user_id;
