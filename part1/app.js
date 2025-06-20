00a8b311e989d7c16678b9eca606f3fb0add3fbd
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:20:43 +0000

    [automatic commit] 2025-06-20 06:20:43 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -119,6 +119,7 @@ app.use(express.static(path.join(__dirname, 'public')));
             `);
         }
 
+        // * Insert records into Users table if table if empty
         [rows] = await db.execute(`SELECT COUNT(*) AS count FROM Dogs`);
         if(rows[0].count === 0){
             await db.execute(`
@@ -143,7 +144,17 @@ app.use(express.static(path.join(__dirname, 'public')));
             `);
         }
 
-        
+        [rows] = await db.execute(`SELECT COUNT(*) AS count FROM WalkRatings`);
+        if(rows[0].count === 0){
+            await db.execute(`
+                INSERT INTO WalkRequests ( dog_id, requested_time, duration_minutes, location, status ) VALUES
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max' LIMIT 1), '2025-06-10 08:00:00', 30, 'Parklands', 'open' ),
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' ),
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Belfort' LIMIT 1), '2025-06-15 08:30:00', 60, 'Versailles', 'open' ),
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Lupin' LIMIT 1), '2025-06-15 09:00:00', 30, 'Versailles', 'accepted' ),
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Fluke' LIMIT 1), '2025-06-18 09:00:00', 45, 'Fort Funston', 'cancelled' );
+            `);
+        }
 
 
 
