a7c03507adde7bc5c764d74cb26785d9e86d2295
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:08:01 +0000

    [automatic commit] 2025-06-20 06:08:01 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -49,6 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')));
             `);
 
         }
+    throw new Error();
     }catch(error){
         process.stdout.write(`A problem occurred when setting up DB for testing!\n\nCheck that DogWalkService Database Exists!\n\n`);
         process.stderr.write(error);
