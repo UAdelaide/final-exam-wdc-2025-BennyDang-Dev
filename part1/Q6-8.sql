f076c9abe8dbd3680caeea21ca70fc2015e4370c
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 08:03:32 +0000

    [automatic commit] 2025-06-20 08:03:32 UTC 203.59.207.151

--- part1/Q6-8.sql
+++ part1/Q6-8.sql
@@ -79,7 +79,7 @@ GROUP BY U.username;
 -- * Maybe we should select a list of walker user names
 -- * Then have two querries selecting completed walks and ratings that is mapped based on username -> object
 -- * We can then process it by looping through the usernames
-SELECT JSON_OBJ(U.username, COUNT(walker_id) AS completed_walks)
+SELECT JSON_OBJECTGG(U.username, COUNT(walker_id) AS completed_walks)
 FROM WalkApplications WA
 INNER JOIN WalkRequests WR ON WA.request_id = WR.request_id
 INNER JOIN Users U on WA.walker_id = U.user_id
