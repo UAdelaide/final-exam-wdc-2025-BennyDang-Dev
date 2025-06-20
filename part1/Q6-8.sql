a91d2565b951e58527ab171a2852229aae844faa
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 07:12:40 +0000

    [automatic commit] 2025-06-20 07:12:40 UTC 203.59.207.151

--- part1/Q6-8.sql
+++ part1/Q6-8.sql
@@ -42,5 +42,5 @@ FROM WalkRequests WR
 INNER JOIN Dogs D ON WR.dog_id = D.dog_id
 INNER JOIN Users U ON D.owner_id = U.user_id;
 
--- * Select walkers with walk requests
+-- * Select walkers with walk ratings;
 SELECT
