bf814bf0f5a07940211ad56447684282f5add889
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 03:55:10 +0000

    [automatic commit] 2025-06-20 03:55:10 UTC 203.59.207.151

--- part1/Q5.sql
+++ part1/Q5.sql
@@ -7,4 +7,5 @@ INSERT INTO Users ( username, email, password_hash, role ) VALUES
     ();
 
 INSERT INTO Dogs ( owner_id, name, size ) VALUES
-    ( (SELECT owner_id FROM Users WHERE username = 'alice123' LIMIT 1), 'Max',  )
+    ( (SELECT owner_id FROM Users WHERE username = 'alice123' LIMIT 1), 'Max', 'medium' ),
+    ( (SELECT owner_id FROM Users WHERE username = 'carol123' LIMIT 1), 'Bella', )
