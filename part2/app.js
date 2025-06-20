0ddd6cb37aca88ba29f914537a0dee534fc87781
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 12:01:53 +0000

    [automatic commit] 2025-06-20 12:01:53 UTC 203.59.207.151

--- part2/app.js
+++ part2/app.js
@@ -5,7 +5,18 @@ require('dotenv').config();
 
 const app = express();
 
-app.get("/",(req,res) => {
+app.use(session({
+    // * Generated randomly from https://random.org
+    secret: 'hZsUxpJ4B8PUNYfHKcDHwf5TJsm2mUZe', // Used to COMPUTE HASH
+    resave: true, // repeat the session or not
+    // have all sessions have a session attached to
+    // them, even if we haven't initialised it
+    saveUninitialized: true,
+    // session cookie works i http
+    cookie: { secure: false }
+}));
+
+app.get("/index.html",(req,res) => {
     const { authenticated, role } = req.session;
     console.log(authenticated, role);
 });
@@ -17,17 +28,6 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-
-app.use(session({
-    // * Generated randomly from https://random.org
-    secret: 'hZsUxpJ4B8PUNYfHKcDHwf5TJsm2mUZe', // Used to COMPUTE HASH
-    resave: true, // repeat the session or not
-    // have all sessions have a session attached to
-    // them, even if we haven't initialised it
-    saveUninitialized: true,
-    // session cookie works i http
-    cookie: { secure: false }
-}));
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
