2f7b2cb1e415c57b40cacf2a67072ddbbfee0529
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 09:03:47 +0000

    [automatic commit] 2025-06-20 09:03:47 UTC 203.59.207.151

--- part2/app.js
+++ part2/app.js
@@ -17,7 +17,7 @@ app.use(session({
     secret: 'string of your choice', // Used to COMPUTE HASH
     resave: false, // repeat the session or not
     // have all sessions have a session attached to
-    //them, even if we haven't initialised it
+    // them, even if we haven't initialised it
     saveUninitialized: true,
     // session cookie works i http
     cookie: { secure: false }
