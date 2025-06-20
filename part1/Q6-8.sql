b090927805e19c0ccb0b78fa8dd7972ca7cd060c
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 07:10:58 +0000

    [automatic commit] 2025-06-20 07:10:58 UTC 203.59.207.151

--- part1/Q6-8.sql
+++ part1/Q6-8.sql
@@ -42,7 +42,7 @@ FROM WalkRequests WR
 INNER JOIN Dogs D ON WR.dog_id = D.dog_id
 INNER JOIN Users U ON D.owner_id = U.user_id;
 
-SELECT COUNT(user_if) FROM
+SELECT COUNT(user_id) FROM
 Users U
 LEFT JOIN WalkRatings WR ON U.user_id = WR.walker_id
 GROUP BY user_id
