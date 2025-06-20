c82330016d2f0f57dce7c25e100813a314ebe468
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 03:58:35 +0000

    [automatic commit] 2025-06-20 03:58:35 UTC 203.59.207.151

--- part1/Q5.sql
+++ part1/Q5.sql
@@ -16,4 +16,4 @@ INSERT INTO Dogs ( owner_id, name, size ) VALUES
 
 -- * Five Walkies
 INSERT INTO WalkRequests ( dog_id, requested_time, duration_minutes, location, status ) VALUES
-    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max' LIMIT 1), )
+    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max' LIMIT 1), '2025-06-10 08' )
