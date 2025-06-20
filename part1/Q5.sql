58ee1a028dffdd7bc7b26aa129b9d2334a11114f
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 03:59:57 +0000

    [automatic commit] 2025-06-20 03:59:57 UTC 203.59.207.151

--- part1/Q5.sql
+++ part1/Q5.sql
@@ -3,7 +3,7 @@ INSERT INTO Users ( username, email, password_hash, role ) VALUES
     ( 'alice123', 'alice@example.com', 'hashed123', 'owner' ),
     ( 'bobwalker', 'bob@example.com', 'hashed456', 'walker' ),
     ( 'carol123', 'carol@example.com', 'hashed789', 'owner '),
-    (),
+    ( '' ),
     ();
 
 -- * Five Doggos
@@ -18,3 +18,7 @@ INSERT INTO Dogs ( owner_id, name, size ) VALUES
 INSERT INTO WalkRequests ( dog_id, requested_time, duration_minutes, location, status ) VALUES
     ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max' LIMIT 1), '2025-06-10 08:00:00', 30, 'Parklands', 'open' ),
     ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' ),
+
+    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' ),
+    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' ),
+    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' );
