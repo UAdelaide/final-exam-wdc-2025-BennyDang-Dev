4de254e4297884ac37411f8a4fca9c02f8763206
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 05:37:18 +0000

    [automatic commit] 2025-06-20 05:37:18 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -15,8 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-let db 
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
