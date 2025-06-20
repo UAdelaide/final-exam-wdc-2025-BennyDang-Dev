9be933bf716617664fb68923550f3368566e34c7
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 08:48:44 +0000

    [automatic commit] 2025-06-20 08:48:44 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -235,7 +235,7 @@ app.get('/api/walkers/summary', async(req,res) => {
             };
             if(typeof completed_walks[username] !== 'undefined'){
                 if(typeof ratings[username] !== 'undefined'){
-                    console.log(ratings[username]);
+                    console.log(ratings[username].);
                     tempObj.total_ratings = ratings[username].total_ratings;
                     tempObj.average_ratings = ratings[username].average_ratings;
                 }
