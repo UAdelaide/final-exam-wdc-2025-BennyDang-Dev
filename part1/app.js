80a552e59e76c9ed7037ae0975a0b2115a899825
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 08:27:33 +0000

    [automatic commit] 2025-06-20 08:27:33 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -199,7 +199,7 @@ app.get('/api/walkers/summary', async(req,res) => {
         `);
     throw new Error();
     }catch(error){
-        res.sendStatus(500).send('A problem occurred!');
+        res.sendStatus(500);
     }
 
 });
