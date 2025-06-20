dbaf85ee282ce3bf8ec0620ec89dd74d85ca8ba5
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 14:37:07 +0000

    [automatic commit] 2025-06-20 14:37:07 UTC 203.59.207.151

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -40,7 +40,7 @@ router.get('/getOwnerDogs', async (req,res) => {
   const query = `SELECT name, dog_id FROM Dogs WHERE owner_id = ?`;
   try {
     // const [rows] = await db.execute(query,user_id);
-    res.status(500).json({ error: 'Failed fecthing dogs' });
+    // res.status(500).json({ error: 'Failed fecthing dogs' });
     // res.json(rows);
   }catch(error){
     res.status(500).json({ error: 'Failed fecthing dogs' });
