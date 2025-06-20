75edc21e54a951ab6a2f7d5adfd391bea8d400a0
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 04:06:25 +0000

    [automatic commit] 2025-06-20 04:06:25 UTC 203.59.207.151

--- part1/Q5.sql
+++ part1/Q5.sql
@@ -12,7 +12,7 @@ INSERT INTO Dogs ( owner_id, name, size ) VALUES
     ( (SELECT owner_id FROM Users WHERE username = 'carol123' LIMIT 1), 'Bella', 'small' ),
     ( (SELECT owner_ud FROM Users WHERE username = 'kinglouisXIV' LIMIT 1), 'Belfort', 'large' ),
     ( (SELECT owner_ud FROM Users WHERE username = 'kinglouisXIV' LIMIT 1), 'Lupin', 'large' ),
-    ( (SELECT owner_ud FROM Users WHERE username = 'alice123' LIMIT 1), 'Fluke', '' );
+    ( (SELECT owner_ud FROM Users WHERE username = 'alice123' LIMIT 1), 'Fluke', 'large' );
 
 -- * Five Walkies
 INSERT INTO WalkRequests ( dog_id, requested_time, duration_minutes, location, status ) VALUES
