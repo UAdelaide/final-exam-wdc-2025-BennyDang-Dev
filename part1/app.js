59c2621fa3b387b52e0fe125eadc9ce87eb0e127
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:02:33 +0000

    [automatic commit] 2025-06-20 06:02:33 UTC 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -35,9 +35,8 @@ app.use(express.static(path.join(__dirname, 'public')));
                 password_hash VARCHAR(255) NOT NULL,
                 role ENUM('owner', 'walker') NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
-                `
-        );
-
+        `);
+        // * Insert records into Users table if table if empty
         const [rows] = await db.execute(`SELECT COUNT(*) AS count FROM Users`);
         if(rows[0].count === 0){
             await db.execute
