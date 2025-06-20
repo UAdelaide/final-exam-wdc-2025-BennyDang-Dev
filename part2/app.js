aa214581e55b302de758b3d35dd8046596eb3e14
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 09:03:07 +0000

    [automatic commit] 2025-06-20 09:03:06 UTC 203.59.207.151

--- part2/app.js
+++ part2/app.js
@@ -13,6 +13,16 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+app.use(session({
+    secret: 'string of your choice', // Used to COMPUTE HASH
+    resave: false, // repeat the session or not
+    // have all sessions have a session attached to
+    //them, even if we haven't initialised it
+    saveUninitialized: true,
+    // session cookie only work in https
+    cookie: {secure: false}
+}));
+
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
