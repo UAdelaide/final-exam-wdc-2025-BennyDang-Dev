4a6df512616b3d307440eb0de2ac06b4ea05a843
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 15:06:08 +0000

    [automatic commit] 2025-06-20 15:06:08 UTC 203.59.207.151

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -20,8 +20,13 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/ownerRequests', async (req, res) => {
-  const { userid } = 
-  const query =
+  const { userid } = req.session;
+  const query = `
+  SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
+      FROM WalkRequests wr
+      JOIN Dogs d ON wr.dog_id = d.dog_id
+      JOIN Users u ON d.owner_id = u.user_id
+      WHERE wr.status = 'open'`
   try{
     const [rows] = await db.query()
   }
