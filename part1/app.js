0dad7772898d0009894f75c501ba2c1c925a4125
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 06:20:23 +0000

    [automatic commit] 2025-06-20 06:20:23 UTC ;; communications error to 208.67.222.222#53: timed out 203.59.207.151

--- part1/app.js
+++ part1/app.js
@@ -134,14 +134,16 @@ app.use(express.static(path.join(__dirname, 'public')));
         [rows] = await db.execute(`SELECT COUNT(*) AS count FROM WalkRequests`);
         if(rows[0].count === 0){
             await db.execute(`
-                INSERT INTO Dogs ( owner_id, name, size ) VALUES
-                    ( (SELECT user_id FROM Users WHERE Users.username = 'alice123' LIMIT 1), 'Max', 'medium' ),
-                    ( (SELECT user_id FROM Users WHERE Users.username = 'carol123' LIMIT 1), 'Bella', 'small' ),
-                    ( (SELECT user_id FROM Users WHERE Users.username = 'kinglouisXIV' LIMIT 1), 'Belfort', 'large' ),
-                    ( (SELECT user_id FROM Users WHERE Users.username = 'kinglouisXIV' LIMIT 1), 'Lupin', 'large' ),
-                    ( (SELECT user_id FROM Users WHERE Users.username = 'alice123' LIMIT 1), 'Fluke', 'large' );
+                INSERT INTO WalkRequests ( dog_id, requested_time, duration_minutes, location, status ) VALUES
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max' LIMIT 1), '2025-06-10 08:00:00', 30, 'Parklands', 'open' ),
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' ),
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Belfort' LIMIT 1), '2025-06-15 08:30:00', 60, 'Versailles', 'open' ),
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Lupin' LIMIT 1), '2025-06-15 09:00:00', 30, 'Versailles', 'accepted' ),
+                    ( (SELECT dog_id FROM Dogs WHERE Dogs.name = 'Fluke' LIMIT 1), '2025-06-18 09:00:00', 45, 'Fort Funston', 'cancelled' );
             `);
         }
+
+        
 
 
 
