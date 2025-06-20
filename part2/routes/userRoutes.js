999ae39b286b6280debbbd350f79cdcce776ce05
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 15:23:33 +0000

    [automatic commit] 2025-06-20 15:23:33 UTC 203.59.207.151

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -79,7 +79,8 @@ router.post('/register', async (req, res) => {
 });
 
 router.get('/me', (req, res) => {
-  if (!req.session.authenticated) {
+  // if (!req.session.authenticated)
+  if (true) {
     return res.status(401).json({ error: 'Not logged in' });
   }
   res.status(401).json({ error: 'Not logged in' });
