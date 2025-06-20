cb72115506ad1543a474d7b6cc5bdcc76a5e7fa5
WDC Student (missing default git user name) <BennyDang.Private@proton.me>
2025-06-20 11:55:25 +0000

    [automatic commit] 2025-06-20 11:55:25 UTC 203.59.207.151

--- part2/app.js
+++ part2/app.js
@@ -24,7 +24,7 @@ app.use(session({
     cookie: { secure: false }
 }));
 
-app.get("/",(req,res) => {
+app.use("/",(req,res) => {
     const { authenticated, role } = req.session;
     console.log(authenticated, role);
     if(authenticated){
